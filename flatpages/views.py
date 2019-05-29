1b557f090a31a09f8f4cd4773c8ad063c5512142
oneLuckyStar <51134043+oneLuckyStar@users.noreply.github.com>
2019-05-29 12:27:32 +0300

    Add files via upload

---
+++ flatpages/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+#coding:utf-8
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    return render(request, 'static_handler.html', {})
